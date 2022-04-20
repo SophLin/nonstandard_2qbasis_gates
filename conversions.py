ba423792b87705ac68037f7a4f544dfcd6f8807a
SophLin <linfuhui1997@gmail.com>
2022-04-19 20:36:46 -0500

    Add files via upload

---
+++ conversions.py
@@ -0,0 +1,130 @@
+import numpy as np
+import cmath
+
+Q = np.asarray(
+        [[1, 0, 0, 1j], [0, 1j, 1, 0], [0, 1j, -1, 0], [1, 0, 0, -1j]]
+    ) / np.sqrt(2)
+Q_H = Q.conj().T
+
+def Makhlin_invariants(U):
+    """
+    Returns the Makhlin invariants G1, G2 of operator U. G1 is complex and G2 is real.
+    """
+    M_B = Q_H@U@Q
+    m = M_B.T@M_B
+    tr_m = m.trace()
+    det_U = np.linalg.det(U)
+    G1 = tr_m**2/(16*det_U)
+    G2 = (tr_m**2 - (m@m).trace())/(4*det_U)
+    return G1, G2
+
+# check if two unitaries are locally equiavalent
+def locally_equivalent(U1,U2, tol = 1e-9):
+    G1_U1, G2_U1 = Makhlin_invariants(U1)
+    G1_U2, G2_U2 = Makhlin_invariants(U2)
+    #print("Makhlin_invariants(U1):",G1_U1, G2_U1.real)
+    #print("Makhlin_invariants(U2):",G1_U2, G2_U2.real)
+    if abs(G1_U1.real - G1_U2.real) > tol:
+        return False
+    if abs(G1_U1.imag - G1_U2.imag) > tol:
+        return False
+    if abs(G2_U1 - G2_U2) > tol:
+        return False
+    return True
+
+def logspec_to_U(logspec):
+    """
+    Returns a matrix U such that the logspec of U's Cartan double is the input
+    """
+    F = np.diag(np.exp([1j*cmath.pi*logspec[0],1j*cmath.pi*logspec[1]
+                 ,1j*cmath.pi*logspec[2],1j*cmath.pi*logspec[3]]))
+    U = Q@F@Q_H
+    return U
+
+def Cartan_double(U):
+    M_B = Q_H@U@Q
+    m = M_B.T@M_B
+    return m
+
+# Calculate logSpec of the Cartan double of U
+def U_to_logspec(U):
+    U /= np.linalg.det(U) ** (1 / 4)
+    cartan_double = Cartan_double(U)
+    evals = np.linalg.eigvals(cartan_double)
+    #print("Eigenvals:",evals)
+    logspec = np.log(evals).imag/np.pi/2
+    #print("log then divide by 2*pi*i",logspec)
+    logspec = np.sort(logspec)
+    logspec = logspec[::-1]
+    #print("Returns",logspec)
+    return logspec
+
+#Convert Weyl chamber coordinates to LogSpec
+#can: Weyl chamber coordinates of a gate 1
+#Returns: logspec, and a flag that indicates whether the LogSpec follows the convention
+def weyl_to_logspec(can):
+    logspec = -1*np.array([can[0]-can[1]+can[2], can[0]+can[1]-can[2],
+                           -1*can[0]-can[1]-can[2], -1.*can[0]+can[1]+can[2]])/2
+    logspec.sort()
+    logspec = logspec[::-1]
+    convention = True #whether the Logspec satisfies the convention
+    if abs(np.sum(logspec)) > 1e-9:
+        convention = False
+    elif logspec[3] < logspec[0] - 1:
+        convention = False
+    #elif logspec[2] + 1/2 <= logspec[0]:
+    #    if abs(logspec[2] + 1/2 - logspec[0]) > 1e-9 or logspec[3] + 1/2 > logspec[1]:
+            #print("Rotate:",logspec)
+    #        logspec = np.array([logspec[2]+1/2,logspec[3]+1/2,logspec[0]-1/2,logspec[1]-1/2])
+            #print("After rotation:",logspec)
+    return logspec, convention
+
+#rotate logspec into an equivalent one
+def rotate(logspec):
+    re = np.array([logspec[2]+1/2,logspec[3]+1/2,logspec[0]-1/2,logspec[1]-1/2])
+    return re
+
+def process_fid(x,y):
+    """Norm of the Hilbert-Schmidt inner-product"""
+    # Note: this is the same as Prakash's fidelity function
+    return np.abs((x.conj() * y).sum()/len(y))
+
+def entangling_power(w):
+    c1 = w[0] * np.pi/2
+    c2 = w[1] * np.pi/2
+    c3 = w[2] * np.pi/2
+    return 1/18 * (3 - (np.cos(4*c1)*np.cos(4*c2) + np.cos(4*c2)*np.cos(4*c3) + np.cos(4*c3)*np.cos(4*c1)))
+
+def in_tetra1(w):#w needs to be an np array
+    """Check if a point is in the tetrahedron between CZ, I, sqrt(iSWAP), sqrt(SWAP)^dag"""
+    p1 = np.array([1/2,0,0])#CNOT/CZ
+    p2 = np.array([3/4,1/4,0]) #sqrt(iSWAP)
+    p3 = np.array([3/4,1/4,1/4]) #sqrt(SWAP)^dag
+    normal = np.cross(p2 - p1, p3 - p1)
+    return np.dot(w-p1,normal) > 0
+
+def in_tetra2(w):#w needs to be an np array
+    """Check if a point is in the tetrahedron between CZ, I, sqrt(SWAP), sqrt(iSWAP)"""
+    p1 = np.array([1/2,0,0]) #CZ/CNOT
+    p2 = np.array([1/4,1/4,0]) #sqrt(iSWAP)
+    p3 = np.array([1/4,1/4,1/4]) #sqrt(SWAP)
+    normal = np.cross(p3-p1,p2-p1)
+    return np.dot(w-p1,normal) > 0
+
+def in_tetra3(w):#w needs to be an np array
+    """Check if a point is in the tetrahedron above the PE polyhedron"""
+    p1 = np.array([1/4,1/4,1/4])#sqrt(SWAP)
+    p2 = np.array([3/4,1/4,1/4])#sqrt(SWAP)^dag
+    p3 = np.array([1/2,1/2,0])#iSWAP
+    normal = np.cross(p1-p3,p2-p3)
+    return np.dot(w-p3,normal) > 0
+
+def weyl_part(w):#w needs to be an np array
+    """Check which part of the weyl chamber a pt belongs to"""
+    if in_tetra1(w):
+        return 1
+    if in_tetra2(w):
+        return 2
+    if in_tetra3(w):
+        return 3
+    return 0 #in this case the point is a PE
---
+++ synthesis.py
@@ -0,0 +1,211 @@
+import numpy as np
+from conversions import weyl_to_logspec, rotate
+
+logspec_CZ = np.array([1/4,1/4,-1/4,-1/4])
+logspec_SWAP = np.array([1/4,1/4,1/4,-3/4])
+logspec_SWAP_r = np.array([3/4, -1/4, -1/4, -1/4])
+can_sqrtSWAP = np.array([1/4,1/4,1/4])
+can_sqrtSWAPd = np.array([3/4,1/4,1/4])
+can_B = np.array([1/2,1/4,0])
+can_SWAP = np.array([1/2,1/2,1/2])
+can_CZ = np.array([1/2,0,0])
+
+# [r,k,a,b,c,d, N] in quantum Littlewood-Richardson coefficients such that N_{ab}^{c,d}(r,k)=1 and r+k = 4
+qlr =   [[1, 3, [0], [0], [0], 0, 1],
+         [1, 3, [0], [1], [1], 0, 1],
+         [1, 3, [0], [2], [2], 0, 1],
+         [1, 3, [0], [3], [3], 0, 1],
+         [1, 3, [1], [1], [2], 0, 1],
+         [1, 3, [1], [2], [3], 0, 1],
+         [1, 3, [1], [3], [0], 1, 1],
+         [1, 3, [2], [2], [0], 1, 1],
+         [1, 3, [2], [3], [1], 1, 1],
+         [1, 3, [3], [3], [2], 1, 1],
+         [2, 2, [0, 0], [0, 0], [0, 0], 0, 1],
+         [2, 2, [0, 0], [1, 0], [1, 0], 0, 1],
+         [2, 2, [0, 0], [1, 1], [1, 1], 0, 1],
+         [2, 2, [0, 0], [2, 0], [2, 0], 0, 1],
+         [2, 2, [0, 0], [2, 1], [2, 1], 0, 1],
+         [2, 2, [0, 0], [2, 2], [2, 2], 0, 1],
+         [2, 2, [1, 0], [1, 0], [2, 0], 0, 1],
+         [2, 2, [1, 0], [1, 0], [1, 1], 0, 1],
+         [2, 2, [1, 0], [1, 1], [2, 1], 0, 1],
+         [2, 2, [1, 0], [2, 0], [2, 1], 0, 1],
+         [2, 2, [1, 0], [2, 1], [2, 2], 0, 1],
+         [2, 2, [1, 0], [2, 1], [0, 0], 1, 1],
+         [2, 2, [1, 0], [2, 2], [1, 0], 1, 1],
+         [2, 2, [1, 1], [1, 1], [2, 2], 0, 1],
+         [2, 2, [1, 1], [2, 0], [0, 0], 1, 1],
+         [2, 2, [1, 1], [2, 1], [1, 0], 1, 1],
+         [2, 2, [1, 1], [2, 2], [2, 0], 1, 1],
+         [2, 2, [2, 0], [2, 0], [2, 2], 0, 1],
+         [2, 2, [2, 0], [2, 1], [1, 0], 1, 1],
+         [2, 2, [2, 0], [2, 2], [1, 1], 1, 1],
+         [2, 2, [2, 1], [2, 1], [2, 0], 1, 1],
+         [2, 2, [2, 1], [2, 1], [1, 1], 1, 1],
+         [2, 2, [2, 1], [2, 2], [2, 1], 1, 1],
+         [2, 2, [2, 2], [2, 2], [0, 0], 2, 1],
+         [3, 1, [0, 0, 0], [0, 0, 0], [0, 0, 0], 0, 1],
+         [3, 1, [0, 0, 0], [1, 0, 0], [1, 0, 0], 0, 1],
+         [3, 1, [0, 0, 0], [1, 1, 0], [1, 1, 0], 0, 1],
+         [3, 1, [0, 0, 0], [1, 1, 1], [1, 1, 1], 0, 1],
+         [3, 1, [1, 0, 0], [1, 0, 0], [1, 1, 0], 0, 1],
+         [3, 1, [1, 0, 0], [1, 1, 0], [1, 1, 1], 0, 1],
+         [3, 1, [1, 0, 0], [1, 1, 1], [0, 0, 0], 1, 1],
+         [3, 1, [1, 1, 0], [1, 1, 0], [0, 0, 0], 1, 1],
+         [3, 1, [1, 1, 0], [1, 1, 1], [1, 0, 0], 1, 1],
+         [3, 1, [1, 1, 1], [1, 1, 1], [1, 1, 0], 1, 1],#below this line is the added part
+        [1, 3, [1], [0], [1], 0, 1],
+        [1, 3, [2], [0], [2], 0, 1],
+        [1, 3, [3], [0], [3], 0, 1],
+        [1, 3, [2], [1], [3], 0, 1],
+        [1, 3, [3], [1], [0], 1, 1],
+        [1, 3, [3], [2], [1], 1, 1],
+        [2, 2, [1, 0], [0, 0], [1, 0], 0, 1],
+        [2, 2, [1, 1], [0, 0], [1, 1], 0, 1],
+        [2, 2, [2, 0], [0, 0], [2, 0], 0, 1],
+        [2, 2, [2, 1], [0, 0], [2, 1], 0, 1],
+        [2, 2, [2, 2], [0, 0], [2, 2], 0, 1],
+        [2, 2, [1, 1], [1, 0], [2, 1], 0, 1],
+        [2, 2, [2, 0], [1, 0], [2, 1], 0, 1],
+        [2, 2, [2, 1], [1, 0], [2, 2], 0, 1],
+        [2, 2, [2, 1], [1, 0], [0, 0], 1, 1],
+        [2, 2, [2, 2], [1, 0], [1, 0], 1, 1],
+        [2, 2, [2, 0], [1, 1], [0, 0], 1, 1],
+        [2, 2, [2, 1], [1, 1], [1, 0], 1, 1],
+        [2, 2, [2, 2], [1, 1], [2, 0], 1, 1],
+        [2, 2, [2, 1], [2, 0], [1, 0], 1, 1],
+        [2, 2, [2, 2], [2, 0], [1, 1], 1, 1],
+        [2, 2, [2, 2], [2, 1], [2, 1], 1, 1],
+        [3, 1, [1, 0, 0], [0, 0, 0], [1, 0, 0], 0, 1],
+        [3, 1, [1, 1, 0], [0, 0, 0], [1, 1, 0], 0, 1],
+        [3, 1, [1, 1, 1], [0, 0, 0], [1, 1, 1], 0, 1],
+        [3, 1, [1, 1, 0], [1, 0, 0], [1, 1, 1], 0, 1],
+        [3, 1, [1, 1, 1], [1, 0, 0], [0, 0, 0], 1, 1],
+        [3, 1, [1, 1, 1], [1, 1, 0], [1, 0, 0], 1, 1]]
+
+"""
+Checks whether the gates corresponding to logspec1 and logspec2 will give us a gate corresponding to logspec3,
+in 1 application of each.
+"""
+def check_synthesis(logspec1,logspec2,logspec3,tol=0.,verbose = False):
+    check = True
+    for r,k,a,b,c,d,_ in qlr:
+        acc = d
+        for i in range(r):
+            acc -= logspec1[k+i-a[i]]
+            acc -= logspec2[k+i-b[i]]
+            acc += logspec3[k+i-c[i]]
+        if acc < 0-tol: # the inequality is not satisfied
+            if verbose:
+                print("Fails with r,k,a,b,c,d = ",r,k,a,b,c,d)
+                print(acc,"<0")
+                check = False
+            else:
+                return False
+    return check
+
+# written 03/02/2022
+def check_synthesis_weyl(w1,w2,w3,tol=0.):
+    ls1,_ = weyl_to_logspec(w1)
+    ls2,_ = weyl_to_logspec(w2)
+    ls3,_ = weyl_to_logspec(w3)
+    ls1_r = rotate(ls1)
+    ls2_r = rotate(ls2)
+    ls3_r = rotate(ls3)
+    eq1 = np.array_equal(ls1,ls1_r)
+    eq2 = np.array_equal(ls2,ls2_r)
+    eq3 = np.array_equal(ls3,ls3_r)
+    flag = check_synthesis(ls1,ls2,ls3,tol=tol)
+    if flag:
+        return True
+    if not eq1:
+        flag = check_synthesis(ls1_r,ls2,ls3,tol=tol)
+        if flag:
+            return True
+    if not eq2:
+        flag = check_synthesis(ls1,ls2_r,ls3,tol=tol)
+        if flag:
+            return True
+    if not eq3:
+        flag = check_synthesis(ls1,ls2,ls3_r,tol=tol)
+        if flag:
+            return True
+    if not eq1 and not eq2:
+        flag = check_synthesis(ls1_r,ls2_r,ls3,tol=tol)
+        if flag:
+            return True
+    if not eq1 and not eq3:
+        flag = check_synthesis(ls1_r,ls2,ls3_r,tol=tol)
+        if flag:
+            return True
+    if not eq2 and not eq3:
+        flag = check_synthesis(ls1,ls2_r,ls3_r,tol=tol)
+        if flag:
+            return True
+    if not eq1 and not eq2 and not eq3:
+        return check_synthesis(ls1_r,ls2_r,ls3_r,tol=tol)
+    return False
+
+def check_SWAP(w1, w2,tol=0.):
+    return check_synthesis_weyl(w1,w2,can_SWAP,tol=tol)
+
+def check_CNOT(w1, w2,tol=0.):
+    return check_synthesis_weyl(w1,w2,can_CZ,tol=tol)
+
+#given a gate, output another gate s.t. they can get SWAP in 2 steps
+def find_complement(weyl):
+    if weyl[0] <= 0.5:
+        v1 = can_sqrtSWAP - can_B
+    else:
+        v1 = can_sqrtSWAPd - can_B
+    v1 = v1/np.linalg.norm(v1)
+    v2 = weyl - can_B
+    proj = can_B + v1*np.dot(v1,v2)
+    return proj - (weyl - proj)
+
+""" # old version
+# given a gate, see whether 3 layers of it can get us SWAP
+def swap_in_3(weyl,tol=0.):
+    comp = find_complement(weyl) # 2 layers of the gate has to get us its complement
+    ls1,_ = weyl_to_logspec(weyl)
+    ls2,_ = weyl_to_logspec(comp)
+    return check_synthesis(ls1, ls1, ls2, tol=tol)
+"""
+
+def swap_in_3(weyl,tol=0.):
+    comp = find_complement(weyl) # 2 layers of the gate has to get us its complement
+    return check_synthesis_weyl(weyl,weyl,comp,tol=tol)
+
+def in_not_swapin3_region(w):#w needs to be an np array
+    """
+    Check if the input point is in one of the 2 regions.
+    The volume for {not SWAP in 3} can be divided into 4 tetrahedra.
+    Here we check for the 2 that include I.
+    """
+    #p0 = np.array([0,0,0]) # I0
+    p1 = np.array([1/2,0,0])#CNOT/CZ
+    #p2 = np.array([1,0,0]) #I1
+    p3 = np.array([1/4,1/4,0]) #sqrt(iSWAP) closer to I0
+    p4 = np.array([3/4,1/4,0]) #sqrt(iSWAP) closer to I1
+    p5 = np.array([1/6,1/6,1/6]) # other vertex, closer to I0
+    p6 = np.array([5/6,1/6,1/6]) # other vertex, closer to I1
+    normal1 = np.cross(p5 - p1, p3 - p1)
+    normal2 = np.cross(p4 - p1, p6 - p1)
+    return np.dot(w-p1,normal1) > 0 or np.dot(w-p1,normal2) > 0
+
+def in_not_czin2_region(w):#w needs to be an np array
+    """
+    Check if the input point is in one of the 2 regions.
+    The volume for {not CZ/CNOT in 2} can be divided into 3 tetrahedra.
+    Here we check for the 2 that include I.
+    """
+    p1 = np.array([1/4,0,0])
+    p2 = np.array([3/4,0,0])
+    p3 = np.array([1/4,1/4,0]) #sqrt(iSWAP) closer to I0
+    p4 = np.array([3/4,1/4,0]) #sqrt(iSWAP) closer to I1
+    p5 = np.array([1/4,1/4,1/4]) # sqrt(SWAP)
+    p6 = np.array([3/4,1/4,1/4]) # sqrt(SWAP)^dag
+    normal1 = np.cross(p5 - p1, p3 - p1)
+    normal2 = np.cross(p4 - p2, p6 - p2)
+    return np.dot(w-p1,normal1) > 0 or np.dot(w-p2,normal2) > 0
